3abe60759534360dcc82dfa081f7326ae63d6ff1
Arielle Bryn Chapin <arielle.bryn@gmail.com>
2020-12-21 10:30:57 -0500

    cleaning up some code

--- scripts/moviegenerator.js
+++ scripts/moviegenerator.js
@@ -1,4 +1,4 @@
-/* STORY DATA */
+/* GENERATOR */
 const pronouns = {
     femme: {
       possessive: "her",
@@ -16,60 +16,6 @@ const pronouns = {
       object: "them",
     },
 };
-const nameActors = [
-    {
-      name: "Mary (Aubrey Plaza)",
-      identity: "femme",
-    },
-    {
-      name: "Dean (Timothee Chalamet)",
-      identity: "masc",
-    },
-    {
-      name: "Moira (Kristen Stewart)",
-      identity: "nb",
-    },
-];
-const adjectives = [
-    "dysfunctional",
-    "cold but needy",
-    "humble",
-    "brilliant but lonely",
-];
-const occupations = [
-    "chess prodigy",
-    "Buzzfeed journalist",
-    "mail carrier",
-    "podcaster",
-    "dating app consultant",
-];
-const occupationsLong = [
-    "an Instagram photographer sacrificing {possessive} art for followers to afford gifts for {possessive} nieces and nephews",
-    "a dieting food stylist trying to maintain {possessive} waistline while getting the season's Tasty videos out in time",
-    "a down-on-{possessive} luck SoundCloud rapper who is counting on {possessive} new Christmas album to revive {possessive} career",
-    "a craft beer maker whose special Christmas brew is the talk of the town",
-    "a Peloton trainer who is trying to help people stay healthy this holiday season"
-]
-const situations = [
-    "wants to impress {possessive} mom with a thriving indoor spearmint garden",
-    "can't seem to find the right girl on Hinge and settle down",
-    "just wants to have the best holiday display for {possessive} kids",
-    "is having trouble getting {possessive} savory popcorn brand off the ground",
-];
-const tjItems = [
-      "half-baked baguette",
-      "orange chicken",
-];
-const classDict = {
-adjective: adjectives,
-nameActor: nameActors,
-occupation: occupations,
-occupationLong: occupationsLong,
-situation: situations,
-tjItem: tjItems,
-};
-
-/* GENERATOR */
 
 let storyInfo = {};
 
@@ -83,39 +29,39 @@ let used = [];
 /* FUNCTIONS */
 
 var getRandomArrayValue = (arr) => {
-return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
 var getUniqueRandomArrayValue = (arr) => {
-let value = getRandomArrayValue(arr);
-while (used.includes(value)) {
-    value = getRandomArrayValue(arr);
-}
+    let value = getRandomArrayValue(arr);
+    while (used.includes(value)) {
+        value = getRandomArrayValue(arr);
+    }
 
-used.push(value);
+    used.push(value);
 
-return value;
+    return value;
 }
 
 var addCorrectPronouns = (str, pnouns) => {
-str = str.replaceAll("{possessive}", pnouns.possessive);
-str = str.replaceAll("{subject}", pnouns.subject);
-return str.replaceAll("{object}", pnouns.object);
+    str = str.replaceAll("{possessive}", pnouns.possessive);
+    str = str.replaceAll("{subject}", pnouns.subject);
+    return str.replaceAll("{object}", pnouns.object);
 }
 
 var replaceTheyMeet = (identity) => {
-let value = "";
-switch(identity) {
-    case "femme":
-        value = "she meets";
-        break;
-    case "masc":
-        value = "he meets";
-        break;
-    case "nb":
-    default:
-        value = "they meet";
-        break;
+    let value = "";
+    switch(identity) {
+        case "femme":
+            value = "she meets";
+            break;
+        case "masc":
+            value = "he meets";
+            break;
+        case "nb":
+        default:
+            value = "they meet";
+            break;
 }
 
 theyMeet.innerHTML = value;
